e68ca8578a18a24d37939dc5157bffd45738778b
FunnyAWM <1142978300@qq.com>
2025-02-02 22:21:06 +0800

    [Just for fun]Added a lot of memes to loading interface.

---
+++ LoadingTitle.h
@@ -0,0 +1,21 @@
+//
+// Created by funnyawm on 25-2-2.
+//
+
+#ifndef LOADINGTITLE_H
+#define LOADINGTITLE_H
+#include <QStringList>
+namespace LOADINGTITLE {
+    const QStringList titleList = {
+        "Happy~happy~happy~",
+        "Never gonna give you up~",
+        "德米安正在织自己的三米大围巾……",
+        "你有没有听到孩子们的悲鸣！",
+        "少女祈祷中……",
+        "少女折寿中……",
+        "从水下第一个生命的萌芽开始……到石器时代的巨型野兽……",
+        "嘿，有的时候您的对手手气正旺，挡都挡不住。",
+        "据我研究向导应该是机器，因为他总是电眼逼人……"
+    };
+}
+#endif // LOADINGTITLE_H
